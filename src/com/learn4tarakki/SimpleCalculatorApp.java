48f4dacacf3bccac655310fdb210d3cf581fe729
learn4tarakki <git.learn4tarakki@gmail.com>
2017-02-19 16:09:27 +0530

    sub operation

--- src/com/learn4tarakki/SimpleCalculatorApp.java
+++ src/com/learn4tarakki/SimpleCalculatorApp.java
@@ -5,9 +5,15 @@ public class SimpleCalculatorApp {
     public static int add(int num1, int num2) {
         return (num1 + num2);
     }
+    
+    public static int sub(int num1, int num2) {
+        return (num1 - num2);
+    }
+    
 
     public static void main(String[] args) {
-        int result = add(2, 4);
+        int result = sub(2, 4);
+        
         System.out.println("Result is: " + result);
     }
 
