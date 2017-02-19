9652f5e9cce44d43d74d740a2fcf6cdc10a192f0
learn4tarakki <git.learn4tarakki@gmail.com>
2017-02-19 15:39:27 +0530

    initial commit

---
+++ src/com/learn4tarakki/SimpleCalculatorApp.java
@@ -0,0 +1,14 @@
+package com.learn4tarakki;
+
+public class SimpleCalculatorApp {
+
+    public static int add(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+    public static void main(String[] args) {
+        int result = add(2, 4);
+        System.out.println("Result is: " + result);
+    }
+
+}
